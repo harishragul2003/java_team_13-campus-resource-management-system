e06b28d017a0049b59fc0034cb41b30d791a618e
harishragul2003 <harishragulk@gmail.com>
2026-02-18 01:15:42 +0530

    Updated API URL to Render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://campus-backend-9q4l.onrender.com/api'
 });
 
 export default api;
