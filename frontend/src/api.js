a43225b6a83dddb6caaa09099de8becc7cf763bf
harishragul2003 <harishragulk@gmail.com>
2026-02-18 13:52:17 +0530

    Update API baseURL configuration

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
-   baseURL: 'http://localhost:5000/api'
+  //  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://campus-backend-9q4l.onrender.com/api'
+
 });
 
 export default api;
